0f580ef2269178467f5550726186a2bd3195a318
Garcia Hurtado <ghurtado@gmail.com>
2014-10-01 02:16:43 -0400

    First version of demo (not working)

---
+++ js/webgl-test.js
@@ -0,0 +1,151 @@
+/**
+ * WebGL Test
+ * 
+ * @autor Garcia Hurtado
+ * @date Oct/1/2014
+ * @link https://developer.mozilla.org/en-US/docs/Web/WebGL/Getting_started_with_WebGL
+ */
+
+// Globals
+var gl;
+var vertexBuffer;
+var vertexPosAttrib;
+var shaderProgram;
+var mvMatrix;
+var perspectiveMatrix;
+
+function start(){
+	gl = initWebGL(document.getElementById("canvas"));
+
+	if(gl){
+		gl.clearColor(0, 0, 0, 1); // fully black
+		gl.clearDepth(1.0);
+		gl.enable(gl.DEPTH_TEST);
+		gl.depthFunc(gl.EQUAL);
+
+		initShaders(); // vertex and pixel shaders
+		initBuffers(); // build geometry
+		setInterval(drawScene, 15);
+	}
+}
+
+/**
+ * Vertex data
+ */
+function initBuffers() {
+  vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  
+  var vertices = [
+    1.0,  1.0,  0.0,
+    -1.0, 1.0,  0.0,
+    1.0,  -1.0, 0.0,
+    -1.0, -1.0, 0.0
+  ];
+  
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
+
+/**
+ * Draw the scene
+ */
+function drawScene() {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+  perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+  
+  loadIdentity();
+  mvTranslate([-0.0, 0.0, -6.0]);
+  
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.vertexAttribPointer(vertexPosAttrib, 3, gl.FLOAT, false, 0, 0);
+  setMatrixUniforms();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+/**
+ * Init Shaders
+ */
+function initShaders(){
+	var vertexShader = getShader("shader-vs");
+	var fragmentShader = getShader("shader-fs");
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Unable to initialize the shader program.");
+  }
+
+  gl.useProgram(shaderProgram);
+
+  vertexPosAttrib = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(vertexPosAttrib);
+}
+
+/**
+ * Init WebGL
+ */	
+function initWebGL(canvas){
+	var gl = null;
+
+	try {
+		gl = canvas.getContext("webgl");
+	} catch(e){
+		// swallow
+	}
+
+	if(!gl){
+		alert("Unable to initialize WebGL");
+	}
+
+	return gl;
+}
+
+/**
+ * Shader Loader
+ */
+function getShader(id) {
+  var shaderScript, source, currentChild, shader;
+  
+  shaderScript = document.getElementById(id);
+  
+  if (!shaderScript) {
+    return null;
+  }
+  
+  source = "";
+  currentChild = shaderScript.firstChild;
+  
+  while(currentChild) {
+    if (currentChild.nodeType == currentChild.TEXT_NODE) {
+      source += currentChild.textContent;
+    }
+    
+    currentChild = currentChild.nextSibling;
+  }
+
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+     // Unknown shader type
+     return null;
+  }
+
+  gl.shaderSource(shader, source);
+    
+  // Compile the shader program
+  gl.compileShader(shader);  
+    
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+      alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));  
+      return null;  
+  }
+    
+  return shader;
+}
